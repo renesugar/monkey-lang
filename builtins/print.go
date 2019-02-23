0b0837128faf7acec14312db23059c187f794766
James Mills <1290234+prologic@users.noreply.github.com>
2019-02-23 17:12:52 +1000

    Run quick short tests in CI

--- builtins/print.go
+++ builtins/print.go
@@ -12,17 +12,11 @@ func Print(args ...object.Object) object.Object {
 	if err := typing.Check(
 		"print", args,
 		typing.MinimumArgs(1),
-		typing.WithTypes(object.STRING),
 	); err != nil {
 		return newError(err.Error())
 	}
 
-	var s string
-	if len(args) == 1 {
-		s = args[0].(*object.String).Value
-	}
-
-	fmt.Println(s)
+	fmt.Println(args[0].String())
 
 	return nil
 }
